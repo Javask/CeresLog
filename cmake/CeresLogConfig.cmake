9cd145aa42c459693aa852981ff9f473193142d8
Michael Jossen <michael.jossen@rwth-aachen.de>
2022-02-08 20:52:57 +0100

    Added CPM, modified CMakeLists.txt to use it, started converting to export correct targets

---
+++ cmake/CeresLogConfig.cmake
@@ -0,0 +1,24 @@
+include(GNUInstallDirs)
+
+
+install(TARGETS CeresLog
+    EXPORT CeresLog-export
+    LIBRARY DESTINATION ${CMAKE_INSTALL_LIBDIR}
+    ARCHIVE DESTINATION ${CMAKE_INSTALL_LIBDIR}
+)
+
+install(EXPORT CeresLog-targets
+  FILE
+    CeresLogTargets.cmake
+  NAMESPACE
+    CeresLog::
+  DESTINATION
+    ${CMAKE_INSTALL_LIBDIR}/cmake/CeresLog
+)
+
+get_filename_component(CeresLog_CMAKE_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH)
+include(CMakeFindDependencyMacro)
+
+if(NOT TARGET CeresLog::CeresLog)
+    include("${CeresLog_CMAKE_DIR}/CeresLogTargets.cmake")
+endif()
